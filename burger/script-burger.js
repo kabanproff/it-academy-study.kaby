3a33d3409a5ade79c23d12dc9bb8f790e1797782
kabanproff <78173194+kabanproff@users.noreply.github.com>
2021-04-06 15:18:45 +0300

    ДОП

--- burger/script-burger.js
+++ burger/script-burger.js
@@ -4,3 +4,5 @@ $(document).ready(function() {
 		$('body').toggleClass('lock');
 	});
 });
+
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
