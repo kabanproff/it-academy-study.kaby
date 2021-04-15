0cbb53985e40241469c5f2046ece3f545a554f23
kabanproff <78173194+kabanproff@users.noreply.github.com>
2021-04-15 20:20:27 +0300

    hh

--- ITPROJECT/js/script-project.js
+++ ITPROJECT/js/script-project.js
@@ -59,3 +59,30 @@ $(document).ready(function() {
     });
   });
 })(jQuery);
+
+$(document).ready(function() {
+  $('.zoom-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: true,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true,
+      titleSrc: function(item) {
+        return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+      }
+    },
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function(element) {
+        return element.find('img');
+      }
+    }
+    
+  });
+});
