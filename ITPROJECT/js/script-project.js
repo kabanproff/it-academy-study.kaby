7ebc4efd9b1ddf00c51cbc96b2c654c5e7c12737
kabanproff <78173194+kabanproff@users.noreply.github.com>
2021-04-16 23:17:26 +0300

    kk

--- ITPROJECT/js/script-project.js
+++ ITPROJECT/js/script-project.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 2000,
   arrows: false,
   pauseOnFocus:false,
   responsive:[
